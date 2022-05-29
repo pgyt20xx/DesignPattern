1f0e53e38190434a587a6d18223e14c247424b5c
yuich <yuich@rogzephyrusm16>
2022-05-30 01:01:29 +0900

    Builder

---
+++ src/Builder/Director.java
@@ -0,0 +1,30 @@
+package Builder;
+
+public class Director {
+	private Builder builder;
+	
+	// コンストラクタ
+	public Director(Builder builder) {
+		this.builder = builder;
+	}
+	
+	// 文章を作るメソッド
+	public void construct() {
+		builder.makeTitle("Greeting");
+		builder.makeString("一般的な挨拶");
+		builder.makeItems(new String[] {
+				"How are you?",
+				"Hello.",
+				"Hi."
+		});
+		
+		builder.makeString("時間帯に応じた挨拶");
+		builder.makeItems(new String[] {
+				"Good morning.",
+				"Good afternoon.",
+				"Good evening."
+		});
+		
+		builder.close();
+	}
+}
